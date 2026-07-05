0aa3ef1ef8a984dec3a7dd8fe00dae318d1aa695
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-07-05 11:23:48 +0900

    Add plugin entry point with debug controls

---
+++ src/RePvP/Plugin.cs
@@ -0,0 +1,70 @@
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace RePvP;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public sealed class Plugin : BaseUnityPlugin
+{
+    public const string PluginGuid = "dev.sakus.repvp";
+    public const string PluginName = "Re-PvP";
+    public const string PluginVersion = "0.1.0";
+
+    internal static ManualLogSource Log { get; private set; } = null!;
+    internal static RePvPConfig ModConfig { get; private set; } = null!;
+    internal static RoundManager RoundManager { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Log = Logger;
+        ModConfig = new RePvPConfig(Config);
+        RoundManager = new RoundManager(ModConfig, new UnityPlayerProvider());
+
+        Logger.LogInfo($"{PluginName} {PluginVersion} loaded.");
+    }
+
+    private void Update()
+    {
+        if (!ModConfig.Enabled.Value)
+        {
+            return;
+        }
+
+        RoundManager.Tick(Time.deltaTime);
+        HandleDebugKeys();
+    }
+
+    private void HandleDebugKeys()
+    {
+        if (!ModConfig.DebugKeysEnabled.Value)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F6))
+        {
+            RoundManager.ForceStartRound();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F7))
+        {
+            RoundManager.AddCashForDebug(ModConfig.DebugCashAmount.Value);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F8))
+        {
+            RoundManager.TriggerExtractionForDebug();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            RoundManager.EndRoundForDebug(Team.Heisters);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            RoundManager.EndRoundForDebug(Team.Hunter);
+        }
+    }
+}
